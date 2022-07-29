f0a54f89b8df22eee1b1c7dea4c7eb57450bbee7
XenderMd <denis.stavilamd@gmail.com>
2022-07-29 15:51:06 +0300

    Scaffolding routes

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/currentUser', ()=>{
-
+router.get('/api/users/currentUser', (req, res)=>{
+    res.send('Hi there!');
 });
 
 export {router as currentUserRouter}
