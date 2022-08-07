60c2c28b84e3202bb75527732b47b8d439035ba5
XenderMd <denis.stavilamd@gmail.com>
2022-08-07 18:44:54 +0300

    Async error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import {json} from 'body-parser';
 
+import { asyncHandler } from './errors/async-error-handler';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -17,9 +18,9 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', ()=>{
+app.all('*', asyncHandler(()=>{
     throw new NotFoundError();
-});
+}));
 
 app.use(errorHandler);
 
