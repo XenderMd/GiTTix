5348bd4ffa8b0b6a9a0c484dca4ff252de31d9d0
XenderMd <denis.stavilamd@gmail.com>
2022-08-13 15:18:50 +0300

    Built the UserDocument, UserModel and the corresponding Schema for Mongoose

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,42 +1,67 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-//An interface that describes the properties required to create a new User
-interface UserAttrs {
-    email:string;
-    password:string;
-}
+// //An interface that describes the properties required to create a new User
+// interface UserAttrs {
+//     email:string;
+//     password:string;
+// }
 
-// An interface that describes the properties that User model has
-interface UserModel extends mongoose.Model<UserDoc>{
-    build(attrs: UserAttrs):UserDoc;
-}
+// // An interface that describes the properties that User model has
+// interface UserModel extends mongoose.Model<UserDoc>{
+//     build(attrs: UserAttrs):UserDoc;
+// }
 
-// An interface taht describes the properties that a User Document has
-interface UserDoc extends mongoose.Document {
-    email:string;
-    password:string;
-}
+// // An interface taht describes the properties that a User Document has
+// interface UserDoc extends mongoose.Document {
+//     email:string;
+//     password:string;
+// }
 
-const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    }
-});
-
-userSchema.statics.build=(attrs:UserAttrs)=>{
-    return new User(attrs);
-}
-
-const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
-
-// const user = User.build({
-//     email:'test@test.com',
-//     password:'password'
+// const userSchema = new mongoose.Schema({
+//     email:{
+//         type:String,
+//         required:true
+//     },
+//     password:{
+//         type:String,
+//         required:true
+//     }
 // });
 
-export {User};
+// userSchema.statics.build=(attrs:UserAttrs)=>{
+//     return new User(attrs);
+// }
+
+// const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+// // const user = User.build({
+// //     email:'test@test.com',
+// //     password:'password'
+// // });
+
+// export {User};
+
+//////////// Officil Mongoose documentation implementation /////////////
+
+import { Schema, model, connect } from 'mongoose';
+
+// 1. Create an interface representing a document in MongoDB.
+interface IUserDocument {
+  email: string;
+  password: string;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUserDocument>({
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+// 3. Create a Model.
+const UserModel = model<IUserDocument>('User', userSchema);
+
+export class User extends UserModel {
+    constructor(params: IUserDocument) {
+      super(params)
+    }
+}
