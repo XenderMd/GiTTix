1a9f9fdd9cee34bb39c7e436877504c1581dc321
XenderMd <denis.stavilamd@gmail.com>
2022-08-27 19:51:24 +0300

    using "getServerSideProps" on the server and on the browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,26 @@
-const indexPage = ()=>{
-    return(
-        <div>Landing page</div>
-    )
-}
+import axios from 'axios';
+ 
+const indexPage = ({ currentUser }) => {
+    console.log(currentUser);
+    return <h1>Hello, {currentUser?.email ?? 'user'}</h1>;
+};
+ 
+export const getServerSideProps = async ({ req }) => {
+    let res;
+    if (typeof window === 'undefined') {
+        //we are on the server
+        res = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',
+            {
+                withCredentials: true,
+                headers: req.headers
+            }
+        );
+    } else {
+        //we are in the browser
+        res = await axios.get('/api/users/current');
+    }
+    return { props: res.data };
+};
 
 export default indexPage;
