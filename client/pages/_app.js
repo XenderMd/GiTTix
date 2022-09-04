671856928b13315c2f1de77f07327b7049a44f22
XenderMd <denis.stavilamd@gmail.com>
2022-09-04 17:04:07 +0300

    Passing props through

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,7 +3,8 @@ import App from 'next/app';
 
 import axiosClient from "../api/build-client";
 
-const AppComponent = ({Component, pageProps})=>{
+const AppComponent = ({Component, pageProps, currentUser})=>{
+    console.log(currentUser);
     return  <div>
                 <h1>Header</h1>
                 <Component {...pageProps}/>
@@ -12,10 +13,12 @@ const AppComponent = ({Component, pageProps})=>{
 
 
 AppComponent.getInitialProps = async appContext =>{
+    // console.log(appContext.Component);
     const appProps = await App.getInitialProps(appContext);
     const client = axiosClient(appContext.ctx)
     const {data} = await client.get('/api/users/currentuser');
-    return {data, ...appProps};
+    // console.log(data);
+    return {...data, ...appProps};
 }
 
 // export const getServerSideProps = async (appContext) => {
