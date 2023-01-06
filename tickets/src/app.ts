051a4bb523023efa4d629d8a7d5537b774b11bc5
XenderMd <denis.stavilamd@gmail.com>
2023-01-06 19:02:27 +0200

    - Added tickets serivce - Added a mongo uri as a env var, during deployment for each individual service

---
+++ tickets/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import {
+  asyncHandler,
+  errorHandler,
+  NotFoundError,
+} from '@dstavila-gittix/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.all(
+  '*',
+  asyncHandler(() => {
+    throw new NotFoundError();
+  })
+);
+
+app.use(errorHandler);
+
+export { app };
