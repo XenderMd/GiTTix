05331e07a8d703186a92dae06c8a733ded39a92f
XenderMd <denis.stavilamd@gmail.com>
2023-02-11 15:15:48 +0200

    Order creation logic

--- orders/src/routes/createOrder.ts
+++ orders/src/routes/createOrder.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
-import { requireAuth, validateRequest } from '@dstavila-gittix/common';
 import { body } from 'express-validator';
+
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@dstavila-gittix/common';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -19,6 +26,21 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
     res.send({});
   }
 );
