c65100ac34ba36976b1b54cdf8aa8aa77ae2bab4
XenderMd <denis.stavilamd@gmail.com>
2023-02-15 20:47:29 +0200

    Cancelling an order

--- orders/src/routes/deleteOrder.ts
+++ orders/src/routes/deleteOrder.ts
@@ -1,9 +1,34 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@dstavila-gittix/common';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { orderId } = req.params;
+      const order = await Order.findById(orderId);
+      if (!order) {
+        throw new NotFoundError();
+      }
+      if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+      }
+      order.status = OrderStatus.Cancelled;
+      order.save();
+      res.send(order);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export { router as deleteOrderRouter };
