c5b37bd4ff198e9901e060aad60d9777843037c7
XenderMd <denis.stavilamd@gmail.com>
2023-02-15 20:10:37 +0200

    Fetching individual orders

--- orders/src/routes/getOrder.ts
+++ orders/src/routes/getOrder.ts
@@ -1,9 +1,36 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@dstavila-gittix/common';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    try {
+      if (mongoose.Types.ObjectId.isValid(req.params.orderId)) {
+        const order = await Order.findById(req.params.orderId).populate(
+          'ticket'
+        );
+        if (!order) {
+          throw new NotFoundError();
+        }
+        if (order.userId !== req.currentUser!.id) {
+          throw new NotAuthorizedError();
+        }
+        res.send(order);
+      } else {
+        throw new BadRequestError('Invalid order ID');
+      }
+    } catch (error) {}
+  }
+);
 
 export { router as getOrderRouter };
