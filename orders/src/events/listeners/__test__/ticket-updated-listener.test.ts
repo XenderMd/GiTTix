f94b4b26810bc1fe4c647ac7f9271dab1fa91dcf
XenderMd <denis.stavilamd@gmail.com>
2023-03-04 14:01:49 +0200

    success case testing

--- orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -32,5 +32,17 @@ const setup = async () => {
   return { msg, data, ticket, listener };
 };
 
-it('finds, updates and saves a ticket', async () => {});
-it('acks the message', async () => {});
+it('finds, updates and saves a ticket', async () => {
+  const { ticket, msg, listener, data } = await setup();
+  await listener.onMessage(data, msg);
+  const updatedTicket = await Ticket.findById(ticket.id);
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+  expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it('acks the message', async () => {
+  const { msg, data, listener } = await setup();
+  await listener.onMessage(data, msg);
+  expect(msg.ack).toHaveBeenCalled();
+});
