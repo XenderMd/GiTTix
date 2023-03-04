daa41e3331a681e20f6dbb02f95cf2828202c434
XenderMd <denis.stavilamd@gmail.com>
2023-03-04 13:44:03 +0200

    Testing the ack call

--- orders/src/events/listeners/__test__/ticket-created-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -37,7 +37,14 @@ it('creates and saves a ticket', async () => {
 });
 
 it('acks the message', async () => {
+  const { data, listener, msg } = await setup();
   // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
   // write assertions to make sure a ticket was created
+  const ticket = await Ticket.findById(data.id);
+  expect(ticket).toBeDefined();
+  expect(ticket?.title).toEqual(data.title);
+  expect(ticket?.price).toEqual(data.price);
   // write assertions to make sure ack is called
+  expect(msg.ack).toHaveBeenCalled();
 });
