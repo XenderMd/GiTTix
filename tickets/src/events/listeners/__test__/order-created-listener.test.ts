5e158199aab53498d26c36c4f134897c138c2e82
XenderMd <denis.stavilamd@gmail.com>
2023-03-05 20:12:18 +0200

    Setup for testing ticket reservation

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import { OrderCreatedEvent, OrderStatus } from '@dstavila-gittix/common';
+import { OrderCreatedListener } from '../orders-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+import { Message } from 'node-nats-streaming';
+
+const setup = async () => {
+  //create an instance of the listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = new Ticket({
+    title: 'Concert',
+    price: 99,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  });
+  await ticket.save();
+
+  // Create the fake data event
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    expiresAt: 'alksjdlkasjdklasj',
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  //@ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
