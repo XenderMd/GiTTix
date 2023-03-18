f295329a5e99a56e65211fdc83b046706e23f69d
XenderMd <denis.stavilamd@gmail.com>
2023-03-18 14:37:16 +0200

    Queueing a job on Event Arrival

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,10 +1,18 @@
 import { Message } from 'node-nats-streaming';
+
 import { Subjects, Listener, OrderCreatedEvent } from '@dstavila-gittix/common';
 import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
+  }
 }
