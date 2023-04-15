92a85a76035b52659e10d080fd0209edce1a8ed6
XenderMd <denis.stavilamd@gmail.com>
2023-04-15 20:26:15 +0300

    Emitting the order cancelled event

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,32 @@
+import { Message } from 'node-nats-streaming';
+
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+import { Order } from '../../models/order';
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  Subjects,
+  OrderStatus,
+} from '@dstavila-gittix/common';
+import { queueGroupName } from './queue-group-name';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId);
+    if (!order) {
+      throw new Error('Order not found');
+    }
+    order.set({ status: OrderStatus.Cancelled });
+
+    await order.save();
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version!,
+    });
+
+    msg.ack();
+  }
+}
