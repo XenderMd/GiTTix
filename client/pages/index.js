e5ad49cfb5fd5e26de3280b8a7c4c4f62d2efe08
XenderMd <denis.stavilamd@gmail.com>
2023-06-05 18:31:16 +0300

    Listing all tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,34 @@
 import axiosClient from '../api/build-client';
 
-const indexPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are not signed in</h1>
+const indexPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
 export const getServerSideProps = async (context) => {
-  //   const client = axiosClient(context);
-  //   const { data } = await client.get('/api/users/currentUser');
-  //   return { props: data };
-  const data = {};
-  return { props: data };
+  const client = axiosClient(context);
+  const { data } = await client.get('/api/tickets');
+  return { props: { tickets: data } };
 };
 
 export default indexPage;
