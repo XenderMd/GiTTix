0ad5f348036f954bd302380ae9d68cd9b089440d
XenderMd <denis.stavilamd@gmail.com>
2023-06-05 16:55:57 +0300

    Ticket Creation

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,33 +1,33 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-const useRequest = ({url, method, body, onSuccess})=>{
-    
-    const [errors, setErrors] = useState(null);
+const useRequest = ({ url, method, body, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
 
-    const doRequest = async ()=>{
-        try {
-            setErrors(null);
-            const response = await axios[method](url, body);
-            
-            if(onSuccess){
-                onSuccess(response.data);
-            }
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
 
-            return response.data;
-        } catch (error) {
-            setErrors(
-                <div className="alert alert-danger">
-                    <h4>Oooops...</h4>
-                    <ul className="my-0">
-                        {error.response.data.map((error)=>{return(<li key={error.message}>{error.message}</li>)})}
-                    </ul>
-                </div>
-            )
-        }
-    };
+      return response.data;
+    } catch (error) {
+      setErrors(
+        <div className='alert alert-danger'>
+          <h4>Oooops...</h4>
+          <ul className='my-0'>
+            {error.response.data.map((error) => {
+              return <li key={error.message}>{error.message}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    }
+  };
 
-    return { errors, doRequest};
-}
+  return { errors, doRequest };
+};
 
 export default useRequest;
